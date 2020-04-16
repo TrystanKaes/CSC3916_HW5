b22fdc7659f1a2feccb01a829fe04e6c57c6f6a6
Trystan Kaes <baby.hughee@gmail.com>
2020-04-15 20:40:07 -0600

    hf

--- src/components/movie.js
+++ src/components/movie.js
@@ -18,12 +18,12 @@ class Movie extends Component {
 
     render() {
         const ActorInfo = (actors) => {
-            return actors.map((actor, i) =>
-                <p key={i}>
-                    <b>{actor.actorName}</b> {actor.characterName}
-                    {/*<b>{actor}</b>*/}
-                </p>
-            )
+            // return actors.map((actor, i) =>
+            //             //     <p key={i}>
+            //             //         <b>{actor.actorName}</b> {actor.characterName}
+            //             //         {/*<b>{actor}</b>*/}
+            //             //     </p>
+            //             // )
         }
 
         const ReviewInfo = (reviews) => {
